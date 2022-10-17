8efc908c1316f4a6d5f1edc9672f863b1204dd7f
Joseph Ke <r96340@gmail.com>
2022-10-17 14:29:11 +0800

    Update to enable extraction of all tables on the page

--- Column-extract.py
+++ Column-extract.py
@@ -1,11 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://en.wikipedia.org/wiki/List_of_cakes"
+url = "https://en.wikipedia.org/wiki/List_of_Romantic_composers"
 
 res = requests.get(url)
 soup = BeautifulSoup(res.text,"lxml")
-for items in soup.find(class_="wikitable").find_all("tr")[1:]:
-    e = items.find_all('td')
-    data = f'{e[0].text.strip()}'
-    print(data)
+for items in soup.find_all('table',class_="wikitable"):
+    for itemss in items.find_all("tr")[1:]:
+        e = itemss.find_all('td')
+        data = f'{e[0].text.strip()}'
+        print(data)
